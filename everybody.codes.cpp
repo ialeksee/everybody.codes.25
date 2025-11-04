91b135ba1268417a3f8bda89249fa6c9ce5eaf68
Ivaylo Alekseev <ivaylo.alekseev@scania.com>
2025-11-04 23:33:46 +0100

    input for the first day added

--- everybody.codes.cpp
+++ everybody.codes.cpp
@@ -2,6 +2,19 @@
 
 int main()
 {
+    int input;
     std::cout << "*************** A Song of Ducks and Dragons ****************** \n";
     std::cout << "Choose a challenge:\n";
+    std::cout << "1. Whispers in the Shell\n";
+    std::cout << "Any other key to quit.\n";
+    std::cin >> input;
+
+    switch(input)
+    {
+        case 1: 
+            std::cout << "placeholder \n";
+        break;
+        default:
+            std::cout << input << std::endl;
+    }
 }
