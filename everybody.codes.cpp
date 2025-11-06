cf360fb2b4aa96d7ad2e7fc8063869dfcc2b6c51
Ivaylo Alekseev <ivaylo.alekseev@scania.com>
2025-11-06 21:20:15 +0100

    small progress on day1.3

--- everybody.codes.cpp
+++ everybody.codes.cpp
@@ -1,6 +1,5 @@
 #include "util/util.h"
-#include <_stdio.h>
-#include <vector>
+
 void chapter1();
 int main()
 {
@@ -127,5 +126,73 @@ void chapter1()
         }
     
         std::cout << "Part 2, my parent's name: " << names.at(index) << std::endl;
+    }
+    {
+        std::vector<std::string> inputVec;
+        Util::readFile("./input/everybody_codes_e2025_q01_p3.txt", inputVec);
+
+        std::vector<std::string> names;
+        {
+            std::stringstream ss(inputVec[0]);
+            while(ss.good())
+            {
+                std::string substr;
+                getline(ss, substr, ',');
+                names.push_back(substr);
+            }
+        }
+        std::stringstream ss(inputVec[1]);
+        std::vector<std::string> instructions;
+        while(ss.good())
+        {
+            std::string substr;
+            getline(ss, substr, ',');
+            instructions.push_back(substr);
+        }
+
+
+
+        int index = 0;
+        for(auto str : instructions)
+        {
+            char direction = str.at(0);
+            int steps = (int)(str.at(1) - 0x30);
+            if(str.size() > 2)
+            {    
+                steps *= 10;
+                steps += (int)(str.at(2) - 0x30);
+                
+            }
+            
+            if(steps >= names.size())
+                steps %= names.size();
+
+            switch(direction)
+            {
+                case 'L':
+                    index -= steps;
+                break;
+                case 'R':
+                    index += steps;
+                break;
+            };
+
+            if(index < 0)
+                index += names.size();
+
+            if(index >= names.size())
+                index -= names.size();
+
+            std::string temp = names.at(0);
+            std::cout << temp << std::endl; 
+            std::cout << "index at: " << index << std::endl;
+            names[0] = names[index];
+            std::cout << names[0] << std::endl; 
+            names[index] = temp;
+             std::cout << names[index] << std::endl; 
+ 
+        }
+    
+        std::cout << "Part 3, my other parent's name: " << names.at(0) << std::endl;
     }
 }
